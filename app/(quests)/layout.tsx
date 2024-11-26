f2499bf23aac68192a592fb9e036d0bda02a9b9e
skuntawat <skuntawat@gmail.com>
2024-11-26 15:02:07 +0700

    update home page

--- app/(quests)/layout.tsx
+++ app/(quests)/layout.tsx
@@ -4,9 +4,11 @@ export default function QuestsLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="max-w-7xl py-20 mx-auto min-h-dvh flex flex-col">
-            <div className="flex-1 flex flex-col items-center justify-center p-6 border rounded-md shadow-md bg-background">
-                {children}
+        <div className="bg-foreground">
+            <div className="max-w-7xl py-20 mx-auto min-h-dvh flex flex-col">
+                <div className="flex-1 flex flex-col items-center justify-center p-6 border rounded-md shadow-md bg-background">
+                    {children}
+                </div>
             </div>
         </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`font-sans antialiased ${fontSans.variable} bg-foreground`}
+        className={`font-sans antialiased ${fontSans.variable}`}
       >
         {children}
       </body>

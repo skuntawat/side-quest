40157fd6f6d712c87e41e448ed027b0d176a2eac
skuntawat <skuntawat@gmail.com>
2025-01-12 16:42:01 +0700

    change to logo black white

--- app/(quests)/(css)/logo-black-white/page.tsx
+++ app/(quests)/(css)/logo-black-white/page.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
-import { cn } from '@/utils/cn'
 
 export default function Page() {
     return (
-        <div className="grid grid-cols-2 gap-4">
-            <div className="bg-[#272822] p-4 rounded-md">
+        <div className="grid grid-cols-3 gap-4">
+            <div className="border border-black p-4 rounded-md">
                 <Image
                     alt='kunkode-logo'
                     src="/images/abstract-logo.png"
                     width={300}
                     height={300}
+                />
+            </div>
+            <div className="border border-black p-4 rounded-md">
+                <Image
+                    alt='kunkode-logo'
+                    src="/images/abstract-logo.png"
+                    width={300}
+                    height={300}
+                    className="brightness-0"
                 />
             </div>
             <div className="bg-[#272822] p-4 rounded-md">
@@ -19,28 +27,7 @@ export default function Page() {
                     src="/images/abstract-logo.png"
                     width={300}
                     height={300}
-                    className={cn(
-                        "brightness-0 invert"
-                    )}
-                />
-            </div>
-            <div className="bg-[#272822] p-4 rounded-md">
-                <Image
-                    alt='kunkode-logo'
-                    src="/images/abstract-logo.png"
-                    width={300}
-                    height={300}
-                />
-            </div>
-            <div className="bg-[#272822] p-4 rounded-md">
-                <Image
-                    alt='kunkode-logo'
-                    src="/images/abstract-logo.png"
-                    width={300}
-                    height={300}
-                    style={{
-                        filter: "brightness(0) invert(1)"
-                    }}
+                    className="brightness-0 invert"
                 />
             </div>
         </div>
